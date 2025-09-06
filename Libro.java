229d2c6e85349631521aa0b346790af82c4ab5bc
Luisa Ordoñez <luisaordonez262@gmail.com>
2025-09-06 12:26:16 -0500

    Update Libro.java

--- Libro.java
+++ Libro.java
@@ -1,67 +1,71 @@
 public class Libro {
- 
     private String titulo;
     private String autor;
-    private int NumeroEjemplares;
-    private int NumeroEjemplaresPrestados;
- 
- 
+    private int numeroEjemplares;
+    private int numeroEjemplaresPrestados;
+
+    
     public Libro() {
-         
+        this.titulo = "";
+        this.autor = "";
+        this.numeroEjemplares = 0;
+        this.numeroEjemplaresPrestados = 0;
     }
- 
-    public Libro(String autor,int  NumeroEjemplares,int  NumeroEjemplaresPrestados){
+
+    
+    public Libro(String autor, int numeroEjemplares, int numeroEjemplaresPrestados) {
+        this.titulo = "";
         this.autor = autor;
-        this.NumeroEjemplares = NumeroEjemplares;
-        this.NumeroEjemplaresPrestados = NumeroEjemplaresPrestados;
+        this.numeroEjemplares = numeroEjemplares;
+        this.numeroEjemplaresPrestados = numeroEjemplaresPrestados;
     }
-   
+
     public String getTitulo() {
         return titulo;
     }
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
- 
+
     public String getAutor() {
         return autor;
     }
     public void setAutor(String autor) {
         this.autor = autor;
     }
- 
- 
+
     public int getNumeroEjemplares() {
-        return NumeroEjemplares;
+        return numeroEjemplares;
     }
- 
-    public void setejemplares(int NumeroEjemplares) {
-        this.NumeroEjemplares = NumeroEjemplares;
+    public void setNumeroEjemplares(int numeroEjemplares) {
+        this.numeroEjemplares = numeroEjemplares;
     }
-   
+
     public int getNumeroEjemplaresPrestados() {
-        return NumeroEjemplaresPrestados;
+        return numeroEjemplaresPrestados;
     }
-    public void setNumeroEjemplaresPrestados(int NumeroEjemplaresPrestados) {
-        this.NumeroEjemplaresPrestados = NumeroEjemplaresPrestados;
+    public void setNumeroEjemplaresPrestados(int numeroEjemplaresPrestados) {
+        this.numeroEjemplaresPrestados = numeroEjemplaresPrestados;
     }
- 
-    boolean prestamo(){
-        if (NumeroEjemplares > NumeroEjemplaresPrestados) {
-            NumeroEjemplaresPrestados = NumeroEjemplaresPrestados + 1;
+
+   
+    public boolean prestamo() {
+        if (numeroEjemplares > numeroEjemplaresPrestados) {
+            numeroEjemplaresPrestados++;
             return true;
         } else {
             return false;
         }
     }
-    boolean devolucion(){
-        if (NumeroEjemplaresPrestados > 0) {
-            NumeroEjemplaresPrestados = NumeroEjemplaresPrestados - 1;
+
+    public boolean devolucion() {
+        if (numeroEjemplaresPrestados > 0) {
+            numeroEjemplaresPrestados--;
             return true;
         } else {
             return false;
         }
     }
 }
- 
+
  
